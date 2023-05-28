7d2cfa4d7fa3b68d0be58523cd4c034b4a520c04
Angus Moore <angusmoore.dev@gmail.com>
2023-05-28 18:45:10 +1000

    loading animation across pages'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,42 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import Head from 'next/head';
 import { ToastContainer } from 'react-toastify';
-import { MantineProvider } from '@mantine/core';
+import { Loader, MantineProvider } from '@mantine/core';
 import { virginDb } from '@/lib/db';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from '@/components/Footer';
+import { Router } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
 	const [supabaseClient] = useState(() => virginDb);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => 
+	{
+		const start = (url: string) => {
+			setLoading(true);
+		};
+		const end = (url: string) => {
+			setLoading(false);
+		};
+		Router.events.on("routeChangeStart", start);
+		Router.events.on("routeChangeComplete", end);
+		Router.events.on("routeChangeError", end);
+
+		window.onbeforeunload = function() {
+			// Currently sessionStorage is only being used for Blob URL caching, so it's safe to use this to revoke URLs
+			sessionStorage.clear();
+		}
+
+		return () => {
+			Router.events.off("routeChangeStart", start);
+			Router.events.off("routeChangeComplete", end);
+			Router.events.off("routeChangeError", end);
+		};
+	}, []);
 
 	return (
 		<SessionContextProvider
@@ -31,7 +57,16 @@ export default function App({ Component, pageProps }: AppProps) {
 						<meta name="description" content="Gehenna is a place for people to document their journeys and share their experiences." />
 					</Head>
 					<div className='flex-grow'>
-						<Component {...pageProps} />
+						{
+							loading &&
+							<div className='w-full h-full flex items-center justify-center'>
+								<Loader />
+							</div>
+						}
+						{
+							!loading &&
+							<Component {...pageProps} />
+						}
 					</div>
 					<ToastContainer
 						position="top-center"
