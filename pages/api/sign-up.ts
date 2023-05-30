9e39e116de4d65b83406a3e9fb9e635b87ba1aaf
Angus Moore <angusmoore.dev@gmail.com>
2023-05-30 17:01:51 +1000

    updated domain

--- pages/api/sign-up.ts
+++ pages/api/sign-up.ts
@@ -79,6 +79,6 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse)
 
 
 const checkEnvironment = () => {
-    let base_url = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://gehenna-mu.vercel.app"; 
+    let base_url = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://www.gehenna.dev"; 
     return base_url;
 };
