e8d66c268d21a260bea2fb38e10be8a7dd55daf8
Angus Moore <angusmoore.dev@gmail.com>
2023-06-02 16:03:11 +1000

    forgot to add profile page

---
+++ components/profile/ProfileBannerManager.tsx
@@ -0,0 +1,180 @@
+import { Profile } from "@/models/Profile";
+import { ImageDropzone } from "../ImageDropzone";
+import Image from "next/image";
+import { useDropzone } from "react-dropzone";
+import { useCallback, useRef, useState } from "react";
+import { toast } from "react-toastify";
+import AvatarEditor from "react-avatar-editor";
+import { Loader, Slider } from "@mantine/core";
+import CommonButton from "../CommonButton";
+import { v4 } from "uuid";
+import { clientDb } from "@/lib/db";
+
+export default function ProfileBannerManager({ profileData }: { profileData: Profile })
+{
+    const ref = useRef<AvatarEditor>(null);
+
+    const [scale, setScale] = useState(1);
+    const [rotate, setRotate] = useState(0);
+    const [isUploading, setIsUploading] = useState(false);
+    const [banner, setBanner] = useState(profileData.profileBannerURL);
+    const [tempBanner, setTempBanner] = useState(profileData.profileBannerURL);
+    const [newProfileBanner, setNewProfileBanner] = useState<File>();
+
+    const onDrop = useCallback(async (acceptedFiles: File[]) => {
+        const newBannerPicture = acceptedFiles[0];
+        if (!newBannerPicture.type.startsWith('image/'))
+        {
+            toast.error('You can only upload images.');
+        }
+        
+        // if the byte size is greater than 4mb, turn em away as well.
+        if (newBannerPicture.size > 4 * 1024 * 1024)
+        {
+            toast.error('Your banner must be less than 4mb.');
+        }
+
+        if (newBannerPicture)
+        {
+            setRotate(0);
+            setScale(1);
+            const url = URL.createObjectURL(newBannerPicture);
+            setTempBanner({
+                url,
+                mimeType: newBannerPicture.type,
+                byteSize: newBannerPicture.size,
+            });
+            setNewProfileBanner(newBannerPicture);
+        }
+        }, [])
+    const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+    return <div className="w-full mx-auto group transition hover:bg-primary hover:text-white p-2 rounded-md">
+        {
+            !isUploading &&
+            <span className="text-2xl font-semibold">Profile Banner</span>
+        }
+        {
+            !isUploading &&
+            banner.url && !newProfileBanner &&
+            <div {...getRootProps()}>
+                <input {...getInputProps()} type='file' accept='image/*' />
+                <Image src={banner.url} quality={100} width={1024} height={256} alt='Profile Banner' className="w-full h-[256px] object-cover rounded-md hover:cursor-pointer" />
+                <span className="mt-2 group-hover:text-white">Drag a New Image or Click to Upload a New Banner</span>
+            </div>
+        }
+        {
+            !isUploading &&
+            !profileData.profileBannerURL.url && !newProfileBanner && // First Time Setup
+            <ImageDropzone onUpload={function (files: File[]) {
+                // Set the temp profile
+                const newBannerPicture = files[0];
+                if (!newBannerPicture.type.startsWith('image/'))
+                {
+                    toast.error('You can only upload images.');
+                }
+
+                // if the byte size is greater than 4mb, turn em away as well.
+                if (newBannerPicture.size > 4 * 1024 * 1024)
+                {
+                    toast.error('Your banner must be less than 4mb.');
+                }
+
+                if (newBannerPicture)
+                {
+                    setRotate(0);
+                    setScale(1);
+                    const url = URL.createObjectURL(newBannerPicture);
+                    setTempBanner({
+                        url,
+                        mimeType: newBannerPicture.type,
+                        byteSize: newBannerPicture.size,
+                    });
+                    setNewProfileBanner(newBannerPicture);
+                }
+            }} isUploading={false} height={200} accept={["image/*"]} />
+        }
+        {
+            !isUploading &&
+            newProfileBanner &&
+            <div className="w-full flex flex-col gap-2 mb-10">
+                <AvatarEditor
+                    ref={ref}
+                    image={tempBanner?.url}
+                    width={768}
+                    height={256}
+                    border={0}
+                    color={[255, 255, 255, 0.6]} // RGBA
+                    scale={scale}
+                    rotate={rotate}
+                    className="w-full h-[256px] object-cover rounded-md"
+                />
+                <div className="flex flex-row gap-2 mx-auto">
+                <button {...getRootProps()} className="">
+                    <input {...getInputProps()} type="file" accept="image/*" />
+                    <CommonButton text='Upload New Image' className="mx-auto" onClick={() => {}} />
+                </button>
+                <CommonButton text='Remove Image' onClick={() => {
+                    setNewProfileBanner(undefined);
+                    URL.revokeObjectURL(tempBanner?.url);
+                }} className="bg-red-500 hover:bg-red-600"/>
+                </div>
+                <div className="mb-4 w-full">
+                    <span>Scale</span>
+                    <Slider value={scale} onChange={(e) => setScale(e)} min={1} max={15} step={0.1} label={scale} />
+                    <span>Rotate</span>
+                    <Slider value={rotate} onChange={(e) => setRotate(e)} min={0} max={360} step={1} label={rotate} />
+                </div>
+                <CommonButton text='Save New Banner' onClick={async () => {
+                    ref.current?.getImage().toBlob(async (blob: Blob | null) => {
+                        setIsUploading(true);
+                        if (blob)
+                        {
+                            if (profileData.profileBannerURL.url)
+                            {
+                                const oldAvatarURL = profileData.profileBannerURL.url.split('profile_pictures/')[1];
+                                await clientDb.storage.from('profile_pictures').remove([oldAvatarURL]);
+                            }
+                            
+                            // Now we add the new one in.
+                            const blobName = `${v4()}.${blob.type.split('/')[1]}`;
+                            await clientDb.storage.from('profile_pictures').upload(`${profileData.id}/${blobName}`, blob,
+                            {
+                                contentType: blob.type,
+                                cacheControl: '3600',
+                                upsert: true
+                            });
+
+                            const url = clientDb.storage.from('profile_pictures').getPublicUrl(`${profileData.id}/${blobName}`).data.publicUrl;
+                            await clientDb.from('profiles').update({
+                                profileBannerURL: {
+                                    url,
+                                    mimeType: blob.type,
+                                    byteSize: blob.size,
+                                }
+                            }).eq('id', profileData.id);
+
+                            setNewProfileBanner(undefined);
+                            URL.revokeObjectURL(tempBanner?.url);
+                            setTempBanner({ url: '', mimeType: '', byteSize: 0 });
+                            setBanner({
+                                url,
+                                mimeType: blob.type,
+                                byteSize: blob.size,
+                            });
+                        }
+                        else
+                        {
+                            toast.error('There was an error uploading, please try again!');
+                        }
+                        setIsUploading(false);
+                    });
+                }} className="bg-green-600 hover:bg-green-500 px-16 mx-auto" />
+            </div>
+        }
+        {
+            isUploading &&
+            <Loader className="mx-auto my-24" size={64} />
+        }
+    </div>
+}
---
+++ pages/profile/[id].tsx
@@ -0,0 +1,235 @@
+import CommonButton from "@/components/CommonButton";
+import { Gehenna } from "@/components/Gehenna";
+import { clientDb, serverDb } from "@/lib/db";
+import { Profile } from "@/models/Profile";
+import { User } from "@supabase/supabase-js";
+import { GetServerSidePropsContext } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+import { IconSettings, IconUser } from "@tabler/icons-react";
+import { toast } from "react-toastify";
+import { Skeleton } from "@mantine/core";
+import { Post } from "@/models/Post";
+import PostPreviewBox from "@/components/PostPreviewBox";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Head from "next/head";
+
+export default function ProfilePage({ me, profile, isFollowing }: { me: User | null, profile: Profile, isFollowing: boolean })
+{
+    const router = useRouter();
+    const { id } = router.query as { id: string };
+    const formattedDate = new Date(profile.createdAt).toLocaleDateString('en-au', { dateStyle: 'long' });
+    const [followers, setFollowers] = useState<number>();
+    const [following, setFollowing] = useState(isFollowing);
+    const [postCount, setPostCount] = useState(50);
+	const [posts, setPosts] = useState<Post[]>();
+	const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        clientDb.from('followers').select('*', { count: 'exact', head: true }).eq('followingId', profile.id).then(({ count }) => {
+            if (count)
+            {
+                setFollowers(count);
+            }
+            else
+            {
+                setFollowers(0);
+            }
+        });
+    }, []);
+
+    useEffect(() => {
+		// We do client side fetching to improve first time load and also to make sure that the user is logged in.
+
+		// generate two timestamptz, one right now and one from a week ago.
+		// then we can use the week ago timestamp to get the posts from the last week.
+
+		const weekAgo = new Date();
+		weekAgo.setDate(weekAgo.getDate() - 7);
+
+		clientDb.from('post').select('*').limit(postCount).order('createdAt', { ascending: false }).then(async res => {
+			if (!res.error && res.data)
+			{
+				setPosts(res.data as Post[]);
+			}
+			else if (res.error)
+			{
+				toast.error(res.error.message);
+			}
+		});
+
+	}, []);
+
+    return <div className="flex-grow w-full flex flex-col gap-4 mx-auto py-16 items-center max-w-4xl">
+        <Head>
+            <title>Gehenna - {profile.username}</title>
+            <link href='/favicon.png' rel='icon' />
+            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+            <meta name="description" content={profile.bio} />
+            <meta property="og:url" content={`https://www.gehenna.dev/profile/${profile.id}`} />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={`Gehenna - ${profile.username}`} />
+            <meta property="og:description" content={`Gehenna - ${profile.bio}`} />
+            <meta property="og:image" content={profile.avatar} />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta property="twitter:domain" content="gehenna.dev" />
+            <meta property="twitter:url" content={`https://www.gehenna.dev/profile/${profile.id}`} />
+            <meta name="twitter:title" content={`Gehenna - ${profile.username}`} />
+            <meta name="twitter:description" content={profile.bio} />
+            <meta name="twitter:image" content={profile.avatar} />
+        </Head>
+        <Link href='/'>
+            <Gehenna />
+        </Link>
+        <div className="w-full relative mb-64">
+            <section className="w-full flex flex-row gap-2 items-end flex-wrap p-2 absolute">
+                <section className="w-full flex-row gap-2 items-end flex-wrap p-2 relative">
+                    {
+                        !profile.profileBannerURL.url &&
+                        <section className="w-full h-full absolute top-0 left-0 rounded-md bg-gradient-to-b from-black to-secondary z-10" />
+                    }
+                    {
+                        profile.profileBannerURL.url &&
+                        <Image id='profileCardBanner' src={profile.profileBannerURL.url} width={1000} height={256} alt="Profile Banner" className="object-cover rounded-md w-full h-[256px] absolute z-0" />
+                    }
+                    <section className="flex flex-row gap-4 items-center flex-wrap z-20 absolute top-32 left-4">
+                        <Image src={profile.avatar} width={100} height={100} alt="Profile Picture" className="object-cover rounded-md w-[100px] h-[100px]" />
+                        <div className="flex flex-col gap-[1px]">
+                            <div className="flex flex-row gap-2 items-center">
+                                <span className="text-2xl font-semibold">{profile.username}</span>
+                                {
+                                    me?.id === profile.id &&
+                                    <Link href='/profile'>
+                                        <IconSettings size={20} className="transition hover:text-primary-light" />
+                                    </Link>
+                                }
+                            </div>
+                            <small className="font-light">{profile.handle}</small>
+                            <span className="text-neutral-200 text-sm max-w-[325px]">{profile.bio}</span>
+                            <div className="flex flex-row items-end gap-4">
+                                <span className="text-sm font-semibold text-neutral-100"><span className="text-neutral-400 text-xs">Joined on&nbsp;<br /></span>{formattedDate}</span>
+                                {
+                                    followers === undefined &&
+                                    <Skeleton width={96} height='' className="rounded-md" />
+                                }
+                                {
+                                    followers !== undefined &&
+                                    <div className="flex flex-row items-center gap-2">
+                                        <IconUser size='20' />
+                                        <span className="text-sm font-semibold">{followers?.toLocaleString()} Follower{followers === 0 || followers > 1 && 's'}</span>
+                                        {
+                                            me && me.id !== profile.id &&
+                                            <CommonButton text={following ? 'Unfollow' : 'Follow'} onClick={async () => {
+                                                if (!following)
+                                                {
+                                                    // Follow
+                                                    const res = await clientDb.from('followers').insert({ followerId: me?.id, followingId: profile.id });
+                                                    if (!res.error)
+                                                    {
+                                                        setFollowing(true);
+                                                        setFollowers(followers! + 1);
+                                                    }
+                                                    else toast.error(res.error.message);
+                                                }
+                                                else
+                                                {
+                                                    // Unfollow
+                                                    const res = await clientDb.from('followers').delete().eq('followerId', me?.id).eq('followingId', profile.id);
+                                                    if (!res.error)
+                                                    {
+                                                        setFollowing(false);
+                                                        setFollowers(followers!- 1);
+                                                    }
+                                                    else toast.error(res.error.message);
+                                                }
+                                            }} className="text-xs py-0 p-1 h-6" />
+                                        }
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                    </section>
+                </section>
+            </section>
+        </div>
+        <div className='w-full h-full mt-10'>
+            {
+                posts &&
+                posts.length === 0 &&
+                <div className='w-full h-full flex flex-col items-center gap-4'>
+                    <h1 className='text-2xl font-bold'>There are no recent posts...</h1>
+                </div>
+            }
+            {
+                posts && posts.length > 0 &&
+                <>
+                <InfiniteScroll 
+                dataLength={posts.length} 
+                next={async () => {
+                    clientDb.from('post').select('*').limit(postCount + 2).order('createdAt', { ascending: false }).then(async res => {
+                        if (!res.error && res.data)
+                        {
+                            setPosts(res.data as Post[]);
+                            setPostCount(postCount + 2);
+                        }
+                        else
+                        {
+                            toast.error(res.error.message);
+                        }
+                    })
+                }} 
+                hasMore={true} 
+                loader={<div className='w-full h-full flex items-center justify-center flex-col gap-2' />} 
+                style={{
+                    width: '100%',
+                    overflow: 'hidden',
+                }}
+                endMessage={
+                    <p style={{ textAlign: 'center' }}>
+                        <b>Yay! You have seen it all</b>
+                    </p>
+                }>
+                    {
+                        posts && posts.length > 0 &&
+                        <div className=' flex flex-row flex-wrap justify-center gap-10'>
+                            {
+                                posts.map((post, index) => <PostPreviewBox key={index} post={post} />)
+                            }
+                        </div>
+                    }
+                </InfiniteScroll>
+                </>
+            }
+        </div>
+    </div>
+}
+
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) =>
+{
+    const supabase = serverDb(context);
+    const me = (await supabase.auth.getUser()).data.user;
+    const profile = (await supabase.from('profiles').select('*').eq('id', context.params?.id).single()).data as Profile | null;
+
+    let isFollowing: boolean = false;
+    // If the profile I'm viewing is not mine, check if I am following this person.
+    if (profile?.id !== me?.id)
+    {
+        const isFollowingRes = await supabase.from('followers').select('*').eq('followerId', me?.id).eq('followingId', profile?.id).single();
+        if (!isFollowingRes.error && isFollowingRes.data)
+        {
+            isFollowing = true;
+        }
+    }
+
+    return {
+        props: {
+            me,
+            profile,
+            isFollowing
+        }
+    }
+}
