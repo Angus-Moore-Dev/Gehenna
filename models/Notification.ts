539bc28439915b0cb8da9297e2d8ba8909057c7f
Angus Moore <angusmoore.dev@gmail.com>
2023-10-31 17:52:05 +1000

    removed needless code

--- models/Notification.ts
+++
@@ -1,9 +0,0 @@
-export type Notification = {
-    id: number;
-    created_at: string;
-    title: string;
-    text: string;
-    link: string;
-    userId: string; // you!
-    seen: boolean; // false in the db if left empty.
-}
--- pages/404.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound()
-{
-    return <div className="flex-grow w-full flex flex-col gap-4 items-center justify-center ">
-        <p className="text-4xl">Page Not Found</p>
-        <p>This page does not exist.</p>
-    </div>
-}
