b914cf3c324eba8724928d6b6de79f10b3b493fd
Angus Moore <angusmoore.dev@gmail.com>
2023-11-13 00:48:22 +1000

    updated 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 export default function NotFound()
 {
-    return <div className="flex-grow w-full flex flex-col gap-4 items-center justify-center ">
+    return <div className="w-full flex-grow flex flex-col gap-4 max-w-3xl mx-auto py-8 items-center">
         <Link href='/'>
             <Gehenna />
         </Link>
-        <p className="text-4xl">Page Not Found</p>
-        <p>This page does not exist.</p>
+        <div className="flex-grow flex flex-col items-center justify-center gap-2">
+            <p className="text-4xl">Page Not Found</p>
+            <p>This page does not exist.</p>
+        </div>
     </div>
 }
